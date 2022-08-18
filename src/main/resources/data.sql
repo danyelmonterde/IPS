e531fa0e96a9604f9068087be8cb6061de29214d
Daniel Monterde <dn.monterde@gmail.com>
2022-08-19 07:20:49 +0800

    fix master data controller

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO iqwddbv3.user (id, username, password,is_admin,is_super_admin)
+INSERT INTO user (id, username, password,is_admin,is_super_admin)
 VALUES (1,'admin','$2a$10$.10dY4fr8a.yap0v7MbeuuXgB9SCyW1rE8Py2useGh6vOiRE6Afpy',1,1);
